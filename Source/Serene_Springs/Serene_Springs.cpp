22bf9d65943ea925dfc0cd656369d913fd9b6d05
ricmarab <ricardomarabeto@gmail.com>
2025-04-02 17:02:56 +0100

    * first commit git ignore

---
+++ Source/Serene_Springs/Serene_Springs.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Serene_Springs.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Serene_Springs, "Serene_Springs" );
